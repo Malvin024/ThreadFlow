2560629a6516fdcd7f4f44597ad08b5677115f41
Dylan Hansen Wijaya <dylan.hansen096@gmail.com>
2024-11-14 21:12:26 +0700

    Add files via upload
    login

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="/CSS/styles.css">
+</head>
+<body>
+    <div class="login-container">
+        <div class="logo">
+            <img src="/Images/logo.png" alt="Logo" />
+        </div>
+        <h1 class="title">Login to Your Account</h1>
+        <div class="login-box">
+            <form action="#">
+                <input type="text" placeholder="Username" required><br>
+                <input type="password" placeholder="Password" required><br>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
