cda8553cd2195e6357594c0d329c180060573c72
Dylan Hansen Wijaya <dylan.hansen096@gmail.com>
2024-11-14 21:43:12 +0700

    Add files via upload

---
+++ register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register Page</title>
+    <link rel="stylesheet" href="/CSS/styles.css">
+</head>
+<body>
+<div class="register-container">
+        <div class="logo">
+            <img src="/Images/logo.png" alt="Logo" />
+        </div>
+        <h1 class="title">Create Account</h1>
+        <div class="login-box">
+            <form action="#">
+                <input type="text" placeholder="Username" required><br>
+                <input type="password" placeholder="Password" required><br>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
