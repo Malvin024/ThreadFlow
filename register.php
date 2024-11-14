50ff3433cd5ee7e4341afa91a79cc5dbf45b1486
Dylan Hansen Wijaya <dylan.hansen096@gmail.com>
2024-11-15 00:43:49 +0700

    Update register.php

--- register.php
+++ register.php
@@ -45,7 +45,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ThreadFlow - Register</title>
-    <link rel="stylesheet" href="CSS/styles2.css">
+    <link rel="stylesheet" href="CSS/styles.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap">
 </head>
 <body>
