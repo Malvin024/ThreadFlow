b3d7ae666a673a04a6116f318ce8893a98db8e1b
Dylan Hansen Wijaya <dylan.hansen096@gmail.com>
2024-11-14 21:37:23 +0700

    Update login.php
    jadi punya brian

--- login.php
+++ login.php
@@ -3,20 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-    <link rel="stylesheet" href="/CSS/styles.css">
+    <title>ThreadFlow - Login</title>
+    <link rel="stylesheet" href="/CSS/styles2.css">
 </head>
 <body>
     <div class="login-container">
         <div class="logo">
-            <img src="/Images/logo.png" alt="Logo" />
+            <img src="/Images/logo.png" alt="ThreadFlow Logo">
         </div>
-        <h1 class="title">Login to Your Account</h1>
         <div class="login-box">
-            <form action="#">
-                <input type="text" placeholder="Username" required><br>
-                <input type="password" placeholder="Password" required><br>
+            <h1>Welcome to ThreadFlow</h1>
+            <p class="subtitle">Connect & Collaborate</p>
+            <form action="login.php" method="post">
+                <input type="text" name="username" placeholder="Username" required>
+                <input type="password" name="password" placeholder="Password" required>
                 <button type="submit">Login</button>
+                <div class="extra-links">
+                    <a href="#">Forgot Password?</a>
+                    <span>|</span>
+                    <a href="#">Sign Up</a>
+                </div>
             </form>
         </div>
     </div>
